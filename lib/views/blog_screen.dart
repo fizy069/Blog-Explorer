9c6ca639027b5e3cc44464904bc5de0e4cb97ff5
fizy069 <mahirg4@gmail.com>
2024-08-08 18:59:12 +0530

    UI enhancement

--- lib/views/blog_screen.dart
+++ lib/views/blog_screen.dart
@@ -10,19 +10,68 @@ class BlogDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(blog.title),
+        title: Text(blog.title, style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.teal,
+        centerTitle: true,
+        elevation: 0,
       ),
-      body: Column(
-        children: [
-          Image.network(blog.imageUrl, fit: BoxFit.cover),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              blog.title,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Hero(
+              tag: blog.imageUrl,
+              child: Image.network(
+                blog.imageUrl,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 250,
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    blog.title,
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal[900],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Published on 08/08/2024',  // Assuming you have a publishedDate property
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula orci nec urna vehicula, nec dignissim justo viverra. Nullam interdum, est nec aliquet varius, nunc ipsum lacinia arcu, at fermentum velit ligula eget lorem. Donec nec turpis sed erat ullamcorper sollicitudin non sit amet lacus. Vivamus eu quam at nulla euismod fermentum non id ligula. Aenean auctor, justo in malesuada auctor, nisi erat placerat lorem, id lobortis eros felis in turpis. Curabitur tincidunt mauris et sem varius, id congue tortor vehicula.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      height: 1.5,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Suspendisse potenti. Morbi non dui eget metus aliquet tincidunt. Pellentesque at vestibulum velit. Fusce ac nisi non justo lacinia efficitur. Aliquam erat volutpat. Nullam eu massa arcu. Etiam pharetra eros a velit dictum, ut dignissim neque tincidunt. Cras interdum lacus at tortor condimentum tincidunt.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      height: 1.5,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,3 @@
-//needs ui changes
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../state/bloc_blog.dart';
@@ -9,33 +8,97 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Blog Explorer'),
+        title: Text('Blog Explorer', style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.teal,
+        centerTitle: true,
+        elevation: 0,
       ),
       body: BlocBuilder<BlogBloc, BlogState>(
         builder: (context, state) {
           if (state is BlogLoading) {
             return Center(child: CircularProgressIndicator());
           } else if (state is BlogLoaded) {
-            return ListView.builder(
-              itemCount: state.blogs.length,
-              itemBuilder: (context, index) {
-                final blog = state.blogs[index];
-                return ListTile(
-                  title: Text(blog.title),
-                  leading: Image.network(blog.imageUrl),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => BlogDetailScreen(blog: blog),
-                      ),
-                    );
-                  },
-                );
+            return RefreshIndicator(
+              onRefresh: () async {
+                context.read<BlogBloc>().add(FetchBlogs());
               },
+              child: ListView.builder(
+                itemCount: state.blogs.length,
+                itemBuilder: (context, index) {
+                  final blog = state.blogs[index];
+                  return Card(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(15),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BlogDetailScreen(blog: blog),
+                          ),
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                blog.imageUrl,
+                                width: 80,
+                                height: 80,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(width: 15),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    blog.title,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.teal[900],
+                                    ),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    blog.title,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey[700],
+                                    ),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             );
           } else if (state is BlogError) {
-            return Center(child: Text(state.message));
+            return Center(
+              child: Text(
+                state.message,
+                style: TextStyle(color: Colors.red, fontSize: 18),
+              ),
+            );
           }
           return Center(child: Text('Press the button to fetch blogs'));
         },
